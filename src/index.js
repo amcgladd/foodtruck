3ab1ff974f4709d62b73089a7987dfe197bc8179
Student <example@example.com>
2018-09-27 11:39:29 -0700

    Build mobile in HTML & CSS.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './sass/styles.scss';
+
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+  });
+  setTimeout(autoplay, 4500);
+  function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+  }
